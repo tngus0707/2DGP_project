e10df1993062c7e07ace6c03c4065a8e2c44f4ae
tngus0707 <rldrkd07@naver.com>
2021-10-21 00:38:33 +0900

    공격 2종

--- 2020182025.py
+++ 2020182025.py
@@ -8,6 +8,7 @@ def handle_events():
     global dirY
     global UDLR
     global knife_pos
+    global attack_pos
 
     events = get_events()
     for event in events:
@@ -28,6 +29,10 @@ def handle_events():
                 UDLR = 3
             elif event.key == SDLK_k:
                 knife_pos = 1
+                attack_pos = 0
+            elif event.key == SDLK_l:
+                attack_pos = 1
+                knife_pos = 0
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
@@ -43,16 +48,19 @@ def handle_events():
     pass
 
 open_canvas(width, height)
+
 cave = load_image('cave.png')
 character = load_image('animation_sheet.png')
 enemy1 = load_image('enemy1.png')
 knife = load_image('knife.png')
 knifeUP = load_image('knife_up.png')
 knifeDOWN = load_image('knife_down.png')
+attack = load_image('attack.png')
 
 running = True
 UDLR = 0
 knife_pos = 0
+attack_pos = 0
 
 x = 800 // 2
 y = 600 // 2
@@ -62,6 +70,7 @@ enemy1_frame = 0
 knife_frame = 0
 knifeUP_frame = 0
 knifeDOWN_frame = 0
+attack_frame = 0
 dirX = 0
 dirY = 0
 
@@ -73,7 +82,11 @@ while running:
     if UDLR == 0:
 
         if knife_pos == 1:
-            knife.clip_draw(knife_frame * 40, 0, 40, 40, x + 20, y - 10)
+            knife.clip_draw(knife_frame * 40, 0, 40, 40, x + 20, y)
+            update_canvas()
+
+        if attack_pos == 1:
+            attack.clip_draw(attack_frame * 50, 0, 50, 50, x + 50, y - 10)
             update_canvas()
 
         character.clip_draw(frame * 50, 0, 50, 50, x, y) #left, bottom, width, height, x, y
@@ -90,9 +103,12 @@ while running:
 
     if UDLR == 1:
         if knife_pos == 1:
-            knife.clip_draw(knife_frame * 40, 40, 40, 40, x - 30, y - 10)
+            knife.clip_draw(knife_frame * 40, 40, 40, 40, x - 30, y)
             update_canvas()
 
+        if attack_pos == 1:
+            attack.clip_draw(attack_frame * 50, 0, 50, 50, x - 50, y - 10)
+            update_canvas()
 
         character.clip_draw(frame * 50, 100, 50, 50, x, y)
 
@@ -115,6 +131,10 @@ while running:
             knifeUP.clip_draw(knifeUP_frame * 38, 0, 38, 32, x, y + 40)
             update_canvas()
 
+        if attack_pos == 1:
+            attack.clip_draw(attack_frame * 50, 0, 50, 50, x, y + 50)
+            update_canvas()
+
         character.clip_draw(frame * 50, 50, 50, 50, x, y)
         update_canvas()
 
@@ -130,6 +150,10 @@ while running:
     if UDLR == 3:
         if knife_pos == 1:
             knifeDOWN.clip_draw(knifeDOWN_frame * 40, 0, 40, 46, x, y - 40)
+            update_canvas()
+
+        if attack_pos == 1:
+            attack.clip_draw(attack_frame * 50, 0, 50, 50, x, y - 50)
             update_canvas()
 
         character.clip_draw(frame * 50, 150, 50, 50, x, y)
