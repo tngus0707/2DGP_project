40267fa83a0fe0761aced02bb413ba37df035c1c
tngus0707 <rldrkd07@naver.com>
2021-10-20 21:21:55 +0900

    캐릭터 상하좌우 전환

--- 2020182025.py
+++ 2020182025.py
@@ -6,8 +6,9 @@ width, height = 800, 600
 
 def handle_events():
     global running
-    global dir
-    global LR
+    global dirX
+    global dirY
+    global UDLR
 
     events = get_events()
     for event in events:
@@ -15,18 +16,28 @@ def handle_events():
             running = False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
-                dir += 1
-                LR = 1
+                dirX += 5
+                UDLR = 0
             elif event.key == SDLK_LEFT:
-                dir -= 1
-                LR = 0
+                dirX -= 5
+                UDLR = 1
+            elif event.key == SDLK_UP:
+                dirY += 5
+                UDLR = 2
+            elif event.key == SDLK_DOWN:
+                dirY -= 5
+                UDLR = 3
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
-                dir -= 1
+                dirX -= 5
             elif event.key == SDLK_LEFT:
-                dir += 1
+                dirX += 5
+            elif event.key == SDLK_UP:
+                dirY -= 5
+            elif event.key == SDLK_DOWN:
+                dirY += 5
 
     pass
 
@@ -35,39 +46,68 @@ cave = load_image('cave.png')
 character = load_image('animation_sheet.png')
 
 running = True
-LR = 1
+UDLR = 0
 x = 800 // 2
+y = 600 // 2
 frame = 0
-dir = 0
+dirX = 0
+dirY = 0
 
 while running:
     clear_canvas()
     cave.draw(width // 2, height // 2)
 
-    if LR == 1:
+    if UDLR == 0:
 
-        character.clip_draw(frame * 50, 0, 50, 50, x, 90) #left, bottom, width, height, x, y
+        character.clip_draw(frame * 50, 0, 50, 50, x, y) #left, bottom, width, height, x, y
         update_canvas()
 
         clear_canvas()
         cave.draw(width // 2, height // 2)
 
         frame = (frame + 1) % 10
-        x += dir * 5
+        x += dirX
         delay(0.03)
 
         handle_events()
 
-    if LR == 0:
+    if UDLR == 1:
 
-        character.clip_draw(frame * 50, 100, 50, 50, x, 90)
+        character.clip_draw(frame * 50, 100, 50, 50, x, y)
         update_canvas()
 
         clear_canvas()
         cave.draw(width // 2, height // 2)
 
         frame = (frame + 1) % 10
-        x += dir * 5
+        x += dirX
+        delay(0.03)
+
+        handle_events()
+
+    if UDLR == 2:
+
+        character.clip_draw(frame * 50, 50, 50, 50, x, y)
+        update_canvas()
+
+        clear_canvas()
+        cave.draw(width // 2, height // 2)
+
+        frame = (frame + 1) % 10
+        y += dirY
+        delay(0.03)
+
+        handle_events()
+
+    if UDLR == 3:
+        character.clip_draw(frame * 50, 150, 50, 50, x, y)
+        update_canvas()
+
+        clear_canvas()
+        cave.draw(width // 2, height // 2)
+
+        frame = (frame + 1) % 10
+        y += dirY
         delay(0.03)
 
         handle_events()
